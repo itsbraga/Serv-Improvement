eeaaf68f5b8b391b9e8d447c6f7993307cb0bae7
adesgran <adesgran.dev@gmail.com>
2021-08-19 11:47:38 +0200

    update deploy.js

--- message_commands/deploy.js
+++ message_commands/deploy.js
@@ -21,24 +21,14 @@ module.exports = {
 					client.commands.set(command.data.name, command);
 					console.log(command);
 
-<<<<<<< HEAD
-					if (command.global === "true" && process.env.DEV_MODE !== "true") {
+					if (command.global === "true" &&
+						process.env.NODE_ENV !== "development") {
 						if (!client.application?.owner) await client.application?.fetch();
 						client.application.commands.create(command.data);
 					} else {
-						msg.guild.commands.create(command.data);
+						if (!client.application?.owner) await client.application?.fetch();
+						await set_guilds_commands(client, command);
 					}
-=======
-				if (
-					command.global === "true" &&
-					process.env.NODE_ENV !== "development"
-				) {
-					if (!client.application?.owner) await client.application?.fetch();
-					client.application.commands.create(command.data);
-				} else {
-					if (!client.application?.owner) await client.application?.fetch();
-					await set_guilds_commands(client, command);
->>>>>>> fe2cd9728bef16e21987bdbdb4d34e0ca077ac87
 				}
 			}
 		});
